c44d2f78beb1ebd6b130663eebddb9bc949f53f5
motoshi-suzuki <motoshi.suzukiii@gmail.com>
2023-05-04 08:53:52 +0900

    initial commit

---
+++ TryPopLibraries/TryPopLibrariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryPopLibrariesApp.swift
+//  TryPopLibraries
+//
+//  Created by Motoshi Suzuki on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct TryPopLibrariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
